ca428fc63e7bb0424a091a62e5a375a08346200e
Carlos Roman <carlos@cliche-corp.co.uk>
2016-09-07 13:20:29 +0100

    Updated project to use latest neoutils It now does not use neoism directly. Also added a docker file to fire up the FT neo4j docker image for testing locally.

--- subjects/subjects_service_test.go
+++ subjects/subjects_service_test.go
@@ -1,12 +1,10 @@
 package subjects
 
 import (
+	"github.com/Financial-Times/neo-utils-go/neoutils"
+	"github.com/stretchr/testify/assert"
 	"os"
 	"testing"
-
-	"github.com/Financial-Times/neo-utils-go/neoutils"
-	"github.com/jmcvetta/neoism"
-	"github.com/stretchr/testify/assert"
 )
 
 const (
@@ -21,155 +19,149 @@ const (
 var defaultTypes = []string{"Thing", "Concept", "Classification", "Subject"}
 
 func TestConnectivityCheck(t *testing.T) {
-	assert := assert.New(t)
 	subjectsDriver := getSubjectsCypherDriver(t)
 	err := subjectsDriver.Check()
-	assert.NoError(err, "Unexpected error on connectivity check")
+	assert.NoError(t, err, "Unexpected error on connectivity check")
 }
 
 func TestPrefLabelIsCorrectlyWritten(t *testing.T) {
-	assert := assert.New(t)
 	subjectsDriver := getSubjectsCypherDriver(t)
 
 	alternativeIdentifiers := alternativeIdentifiers{UUIDS: []string{subjectUUID}}
 	subjectToWrite := Subject{UUID: subjectUUID, PrefLabel: prefLabel, AlternativeIdentifiers: alternativeIdentifiers}
 
 	err := subjectsDriver.Write(subjectToWrite)
-	assert.NoError(err, "ERROR happened during write time")
+	assert.NoError(t, err, "ERROR happened during write time")
 
 	storedSubject, found, err := subjectsDriver.Read(subjectUUID)
-	assert.NoError(err, "ERROR happened during read time")
-	assert.Equal(true, found)
-	assert.NotEmpty(storedSubject)
+	assert.NoError(t, err, "ERROR happened during read time")
+	assert.Equal(t, true, found)
+	assert.NotEmpty(t, storedSubject)
 
-	assert.Equal(prefLabel, storedSubject.(Subject).PrefLabel, "PrefLabel should be "+prefLabel)
-	cleanUp(assert, subjectUUID, subjectsDriver)
+	assert.Equal(t, prefLabel, storedSubject.(Subject).PrefLabel, "PrefLabel should be "+prefLabel)
+	cleanUp(t, subjectUUID, subjectsDriver)
 }
 
 func TestPrefLabelSpecialCharactersAreHandledByCreate(t *testing.T) {
-	assert := assert.New(t)
 	subjectsDriver := getSubjectsCypherDriver(t)
 
 	alternativeIdentifiers := alternativeIdentifiers{TME: []string{}, UUIDS: []string{subjectUUID}}
 	subjectToWrite := Subject{UUID: subjectUUID, PrefLabel: specialCharPrefLabel, AlternativeIdentifiers: alternativeIdentifiers}
 
-	assert.NoError(subjectsDriver.Write(subjectToWrite), "Failed to write subject")
+	assert.NoError(t, subjectsDriver.Write(subjectToWrite), "Failed to write subject")
 
 	//add default types that will be automatically added by the writer
 	subjectToWrite.Types = defaultTypes
 	//check if subjectToWrite is the same with the one inside the DB
-	readSubjectForUUIDAndCheckFieldsMatch(assert, subjectsDriver, subjectUUID, subjectToWrite)
-	cleanUp(assert, subjectUUID, subjectsDriver)
+	readSubjectForUUIDAndCheckFieldsMatch(t, subjectsDriver, subjectUUID, subjectToWrite)
+	cleanUp(t, subjectUUID, subjectsDriver)
 }
 
 func TestCreateCompleteSubjectWithPropsAndIdentifiers(t *testing.T) {
-	assert := assert.New(t)
 	subjectsDriver := getSubjectsCypherDriver(t)
 
 	alternativeIdentifiers := alternativeIdentifiers{TME: []string{tmeID}, UUIDS: []string{subjectUUID}}
 	subjectToWrite := Subject{UUID: subjectUUID, PrefLabel: prefLabel, AlternativeIdentifiers: alternativeIdentifiers}
 
-	assert.NoError(subjectsDriver.Write(subjectToWrite), "Failed to write subject")
+	assert.NoError(t, subjectsDriver.Write(subjectToWrite), "Failed to write subject")
 
 	//add default types that will be automatically added by the writer
 	subjectToWrite.Types = defaultTypes
 	//check if subjectToWrite is the same with the one inside the DB
-	readSubjectForUUIDAndCheckFieldsMatch(assert, subjectsDriver, subjectUUID, subjectToWrite)
-	cleanUp(assert, subjectUUID, subjectsDriver)
+	readSubjectForUUIDAndCheckFieldsMatch(t, subjectsDriver, subjectUUID, subjectToWrite)
+	cleanUp(t, subjectUUID, subjectsDriver)
 }
 
 func TestUpdateWillRemovePropertiesAndIdentifiersNoLongerPresent(t *testing.T) {
-	assert := assert.New(t)
 	subjectsDriver := getSubjectsCypherDriver(t)
 
 	allAlternativeIdentifiers := alternativeIdentifiers{TME: []string{}, UUIDS: []string{subjectUUID}}
 	subjectToWrite := Subject{UUID: subjectUUID, PrefLabel: prefLabel, AlternativeIdentifiers: allAlternativeIdentifiers}
 
-	assert.NoError(subjectsDriver.Write(subjectToWrite), "Failed to write subject")
+	assert.NoError(t, subjectsDriver.Write(subjectToWrite), "Failed to write subject")
 	//add default types that will be automatically added by the writer
 	subjectToWrite.Types = defaultTypes
-	readSubjectForUUIDAndCheckFieldsMatch(assert, subjectsDriver, subjectUUID, subjectToWrite)
+	readSubjectForUUIDAndCheckFieldsMatch(t, subjectsDriver, subjectUUID, subjectToWrite)
 
 	tmeAlternativeIdentifiers := alternativeIdentifiers{TME: []string{tmeID}, UUIDS: []string{subjectUUID}}
 	updatedSubject := Subject{UUID: subjectUUID, PrefLabel: specialCharPrefLabel, AlternativeIdentifiers: tmeAlternativeIdentifiers}
 
-	assert.NoError(subjectsDriver.Write(updatedSubject), "Failed to write updated subject")
+	assert.NoError(t, subjectsDriver.Write(updatedSubject), "Failed to write updated subject")
 	//add default types that will be automatically added by the writer
 	updatedSubject.Types = defaultTypes
-	readSubjectForUUIDAndCheckFieldsMatch(assert, subjectsDriver, subjectUUID, updatedSubject)
+	readSubjectForUUIDAndCheckFieldsMatch(t, subjectsDriver, subjectUUID, updatedSubject)
 
-	cleanUp(assert, subjectUUID, subjectsDriver)
+	cleanUp(t, subjectUUID, subjectsDriver)
 }
 
 func TestDelete(t *testing.T) {
-	assert := assert.New(t)
 	subjectsDriver := getSubjectsCypherDriver(t)
 
 	alternativeIdentifiers := alternativeIdentifiers{TME: []string{tmeID}, UUIDS: []string{subjectUUID}}
 	subjectToDelete := Subject{UUID: subjectUUID, PrefLabel: prefLabel, AlternativeIdentifiers: alternativeIdentifiers}
 
-	assert.NoError(subjectsDriver.Write(subjectToDelete), "Failed to write subject")
+	assert.NoError(t, subjectsDriver.Write(subjectToDelete), "Failed to write subject")
 
 	found, err := subjectsDriver.Delete(subjectUUID)
-	assert.True(found, "Didn't manage to delete subject for uuid %", subjectUUID)
-	assert.NoError(err, "Error deleting subject for uuid %s", subjectUUID)
+	assert.True(t, found, "Didn't manage to delete subject for uuid %", subjectUUID)
+	assert.NoError(t, err, "Error deleting subject for uuid %s", subjectUUID)
 
 	p, found, err := subjectsDriver.Read(subjectUUID)
 
-	assert.Equal(Subject{}, p, "Found subject %s who should have been deleted", p)
-	assert.False(found, "Found subject for uuid %s who should have been deleted", subjectUUID)
-	assert.NoError(err, "Error trying to find subject for uuid %s", subjectUUID)
+	assert.Equal(t, Subject{}, p, "Found subject %s who should have been deleted", p)
+	assert.False(t, found, "Found subject for uuid %s who should have been deleted", subjectUUID)
+	assert.NoError(t, err, "Error trying to find subject for uuid %s", subjectUUID)
 }
 
 func TestCount(t *testing.T) {
-	assert := assert.New(t)
 	subjectsDriver := getSubjectsCypherDriver(t)
 
 	alternativeIds := alternativeIdentifiers{TME: []string{tmeID}, UUIDS: []string{subjectUUID}}
 	subjectOneToCount := Subject{UUID: subjectUUID, PrefLabel: prefLabel, AlternativeIdentifiers: alternativeIds}
 
-	assert.NoError(subjectsDriver.Write(subjectOneToCount), "Failed to write subject")
+	assert.NoError(t, subjectsDriver.Write(subjectOneToCount), "Failed to write subject")
 
 	nr, err := subjectsDriver.Count()
-	assert.Equal(1, nr, "Should be 1 subjects in DB - count differs")
-	assert.NoError(err, "An unexpected error occurred during count")
+	assert.Equal(t, 1, nr, "Should be 1 subjects in DB - count differs")
+	assert.NoError(t, err, "An unexpected error occurred during count")
 
 	newAlternativeIds := alternativeIdentifiers{TME: []string{newTmeID}, UUIDS: []string{newSubjectUUID}}
 	subjectTwoToCount := Subject{UUID: newSubjectUUID, PrefLabel: specialCharPrefLabel, AlternativeIdentifiers: newAlternativeIds}
 
-	assert.NoError(subjectsDriver.Write(subjectTwoToCount), "Failed to write subject")
+	assert.NoError(t, subjectsDriver.Write(subjectTwoToCount), "Failed to write subject")
 
 	nr, err = subjectsDriver.Count()
-	assert.Equal(2, nr, "Should be 2 subjects in DB - count differs")
-	assert.NoError(err, "An unexpected error occurred during count")
+	assert.Equal(t, 2, nr, "Should be 2 subjects in DB - count differs")
+	assert.NoError(t, err, "An unexpected error occurred during count")
 
-	cleanUp(assert, subjectUUID, subjectsDriver)
-	cleanUp(assert, newSubjectUUID, subjectsDriver)
+	cleanUp(t, subjectUUID, subjectsDriver)
+	cleanUp(t, newSubjectUUID, subjectsDriver)
 }
 
-func readSubjectForUUIDAndCheckFieldsMatch(assert *assert.Assertions, subjectsDriver service, uuid string, expectedSubject Subject) {
+func readSubjectForUUIDAndCheckFieldsMatch(t *testing.T, subjectsDriver service, uuid string, expectedSubject Subject) {
 
 	storedSubject, found, err := subjectsDriver.Read(uuid)
 
-	assert.NoError(err, "Error finding subject for uuid %s", uuid)
-	assert.True(found, "Didn't find subject for uuid %s", uuid)
-	assert.Equal(expectedSubject, storedSubject, "subjects should be the same")
+	assert.NoError(t, err, "Error finding subject for uuid %s", uuid)
+	assert.True(t, found, "Didn't find subject for uuid %s", uuid)
+	assert.Equal(t, expectedSubject, storedSubject, "subjects should be the same")
 }
 
 func getSubjectsCypherDriver(t *testing.T) service {
-	assert := assert.New(t)
 	url := os.Getenv("NEO4J_TEST_URL")
 	if url == "" {
 		url = "http://localhost:7474/db/data"
 	}
 
-	db, err := neoism.Connect(url)
-	assert.NoError(err, "Failed to connect to Neo4j")
-	return NewCypherSubjectsService(neoutils.StringerDb{db}, db)
+	conf := neoutils.DefaultConnectionConfig()
+	conf.Transactional = false
+	db, err := neoutils.Connect(url, conf)
+	assert.NoError(t, err, "Failed to connect to Neo4j")
+	return NewCypherSubjectsService(db)
 }
 
-func cleanUp(assert *assert.Assertions, uuid string, subjectsDriver service) {
+func cleanUp(t *testing.T, uuid string, subjectsDriver service) {
 	found, err := subjectsDriver.Delete(uuid)
-	assert.True(found, "Didn't manage to delete subject for uuid %", uuid)
-	assert.NoError(err, "Error deleting subject for uuid %s", uuid)
+	assert.True(t, found, "Didn't manage to delete subject for uuid %", uuid)
+	assert.NoError(t, err, "Error deleting subject for uuid %s", uuid)
 }
